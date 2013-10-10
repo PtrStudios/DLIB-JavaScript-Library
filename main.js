74334f60ba4d4749c01e9ea43b86d01117de4ef9
PtrStudios <gandalfthewhite@outlook.com>
2013-10-10 11:26:15 -0500

    Update example and js

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 var $ = {
-	create : function(t1,t2,p,o) {
-		ti.toUpperCase();
+	create : function(t1,t2,p,v,h,c,o) {
 		var i = document.createElement(t1);
-		var ii = i.setAttribute("type",t2);
-		var iii = i.setAttribute("placeholder",p);
+		if (t2 !== undefined && t2 !== 0) {
+			i.setAttribute("type",t2);
+		}
+		if (p !== undefined && p !== 0) {
+			i.setAttribute("placeholder",p);
+		}
+		if (v !== undefined && v !== 0) {
+			i.setAttribute("value",v);
+		}
+		if (h !== undefined && h !== 0) {
+			i.innerHTML = h;
+		}
+		if (o !== undefined && o !== 0) {
+			i.setAttribute("onclick",o);
+		}
+		if (c !== undefined && c !== 0) {
+			i.setAttribute("class",c);
+		}
 		document.body.appendChild(i);
 	}
 }
+
+$.create('p',undefined,undefined,undefined,"the","div");
+
